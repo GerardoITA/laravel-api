0acdf55f6d91d6e8c28a7104fded8130493d622f
GerardoITA <gerardo.giusti@tiscali.it>
2023-02-17 12:36:09 +0100

    esercizio completato

---
+++ frontend/vite-frontend/src/components/MovieList.vue
@@ -0,0 +1,32 @@
+<script>
+import Axios from 'axios';
+import { store } from '../store';
+
+
+export default {
+  name: "MovieList",
+  data() {
+    return {
+      store
+    }
+  },
+  mounted() {
+    Axios.get(store.movieAPI)
+      .then(res => {
+        store.movieList = res.data;
+        console.log(store.movieList)
+      });
+  }
+}
+</script>
+
+<template>
+<div>
+
+</div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+
+</style>
---
+++ frontend/vite-frontend/src/store.js
@@ -0,0 +1,11 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+
+    movieAPI: "http://localhost:8000/api/v1/test",
+    movieList: [],
+    
+
+
+
+}); 
