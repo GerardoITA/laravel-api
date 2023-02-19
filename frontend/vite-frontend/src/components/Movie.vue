c3ca2103407d1c8a05eabbf151916ff5592d093e
GerardoITA <gerardo.giusti@tiscali.it>
2023-02-19 01:19:37 +0100

    esercizio completato

---
+++ frontend/vite-frontend/src/components/Movie.vue
@@ -0,0 +1,33 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: "Movie",
+    props: {
+        nome: String,
+    },
+    data() {
+        return {
+            store
+        }
+    },
+}
+</script>
+
+<template>
+<div class="carta">
+    <h4>{{ nome }}</h4>
+</div>
+</template>
+
+<style>
+.carta {
+  width: 350px;
+  border: 1px solid black;
+  border-radius: 25px;
+  height: 400px;
+  background-color: lightgrey;
+
+
+}
+</style>
--- frontend/vite-frontend/src/components/MovieList.vue
+++ frontend/vite-frontend/src/components/MovieList.vue
@@ -1,32 +1,50 @@
 <script>
 import Axios from 'axios';
 import { store } from '../store';
-
+import Movie from './Movie.vue'
 
 export default {
   name: "MovieList",
+  components: {
+    Movie
+  },
   data() {
     return {
       store
     }
   },
+  methods: {
+    debug() { console.log(store.movieList)}
+
+  },
   mounted() {
     Axios.get(store.movieAPI)
       .then(res => {
-        store.movieList = res.data;
-        console.log(store.movieList)
+        store.movieList = res.data.response;
+        
       });
   }
 }
 </script>
 
 <template>
-<div>
+  <div class="contenitore">
+    <Movie class="cartaa" v-for="movie in store.movieList"
+      :nome="movie.name"
+    >
+    </Movie>
+    <!-- <button @click="debug()">Debug</button> -->
+  </div>
 
-</div>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
-
+.contenitore {
+ display: flex;
+ gap: 25px;
+}
+.cartaa {
+  width: 350px;
+}
 </style>
